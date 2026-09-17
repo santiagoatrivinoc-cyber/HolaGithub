3c6b03c96292377ed91fac28889b6f40aa34c8ac
Pablo Cubillos <pabloa.cubillosf@uqvirtual.edu.co>
2026-09-16 19:19:45 -0500

    Ya Pablo agregó mensaje nuevamente

--- HolaGithub/src/main/java/Main.java
+++ HolaGithub/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String[]args){
         System.out.println("Hola Github");
-        System.out.println("Hola desde Pablo");
+        System.out.println("Hola desde Pablo, aquí pablo de nuevo");
 
     }
 }
